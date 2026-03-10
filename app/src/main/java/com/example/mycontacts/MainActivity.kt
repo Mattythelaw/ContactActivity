37fb566f4d99777f53367b67588f5c8888958539
Mattythelaw <matthewconnor0418@gmail.com>
2026-03-10 11:34:37 +0200

    Added ice task

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -10,14 +10,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        var firstName = "Matthew"
-            var lastName = "Lawrence"
-                var Address = "21 Church Road"
-                    var isMale = true
-                        var dayOfBirth = 18
-                            var monthOfBirth = 4
-                                var yearOfBirth = 2007
-        Log.d("Hello" , "Your name is" + firstName + " " + lastName + " " + "Your Address" + "" + Address + " " + "Are you male" + isMale)
+        val firstName = "Matthew"
+            val lastName = "Lawrence"
+                val address = "21 Church Road"
+                    val isMale = true
+                        val dayOfBirth = 18
+                            val monthOfBirth = 4
+                                val yearOfBirth = 2007
+
+        Log.d("Hello" , "Your name is " + firstName + " " + lastName + " " + "Your Address"
+                + " " + address + " " + "Are you male" + isMale + " " + "Your day of birth "
+                + dayOfBirth + " " + "Your month of birth " + monthOfBirth+ " " + "Birthday year "
+                + yearOfBirth + " ")
+
+        val currentYear = 2026
+        val age = currentYear - yearOfBirth
+        val yearsUntil100 = 100 - age
+        val ageInMonths = age * 12
+        val remainder = age % 5
+        val ageInDays = age * 365
+
+        Log.d("Hello","remainder: $remainder")
+        Log.d("Hello","yearsUntil100: $yearsUntil100")
+        Log.d("Hello","ageInMonths: $ageInMonths")
+        Log.d("Hello","ageInDays: $ageInDays")
+
+
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
